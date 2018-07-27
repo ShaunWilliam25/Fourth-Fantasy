347a815dbc9ec6ceee720469c6c77d3dbdde8e4a
PreetProot <41739104+PreetProot@users.noreply.github.com>
2018-07-27 15:34:50 +0800

    Encyclopedia and main menu tweaking
    Added more items in ency
    removed "currently not available"
    *brightness settings not working still fixing*

--- Assets/NextPage1.cs
+++ Assets/NextPage1.cs
@@ -10,9 +10,14 @@ public class NextPage1 : MonoBehaviour
     public int EncyclopediaP2 = 0;//time
     public int EncyclopediaP3 = 0;//old
     public int EncyclopediaP4 = 0;//noble
-    public int EncyclopediaP5 = 0;//spiritdoor
-    public int EncyclopediaP6 = 0;//wolf
-    public int EncyclopediaP7 = 0;//consuming knight
+    public int EncyclopediaP5 = 0;//sword
+    public int EncyclopediaP6 = 0;//archer
+    public int EncyclopediaP7 = 0;//lance
+    public int EncyclopediaP8 = 0;//wolf
+    public int EncyclopediaP9 = 0;//spiritdoor
+    public int EncyclopediaP10 = 0;//consuming knight
+    public int EncyclopediaP11 = 0;//story
+
     public int StartMenuFinal = 0;//start
 
     public void Encyclopedia_ExileDemon()
@@ -42,22 +47,46 @@ public class NextPage1 : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP4); ///get the next level after pressting next
 	}
 
-    public void Encyclopedia_SpiritDoor()
+    public void Encyclopedia_Sword()
     {
         Debug.Log("EncyclopediaTimePriestessNEXT");
         UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP5); ///get the next level after pressting next
 	}
 
-    public void Encyclopedia_Mimic()
+    public void Encyclopedia_Archer()
     {
         Debug.Log("EncyclopediaTimePriestessNEXT");
         UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP6); ///get the next level after pressting next
 	}
 
-    public void Encyclopedia_ConsumingKnight()
+    public void Encyclopedia_Lance()
     {
         Debug.Log("EncyclopediaTimePriestessNEXT");
         UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP7); ///get the next level after pressting next
+	}
+
+    public void Encyclopedia_Wolf()
+    {
+        Debug.Log("EncyclopediaTimePriestessNEXT");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP8); ///get the next level after pressting next
+	}
+
+    public void Encyclopedia_SpiritDoor()
+    {
+        Debug.Log("EncyclopediaTimePriestessNEXT");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP9); ///get the next level after pressting next
+	}
+
+    public void Encyclopedia_ConsumingKnight()
+    {
+        Debug.Log("EncyclopediaTimePriestessNEXT");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP10); ///get the next level after pressting next
+	}
+
+    public void Encyclopedia_Story()
+    {
+        Debug.Log("EncyclopediaTimePriestessNEXT");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP11); ///get the next level after pressting next
 	}
 
     //-----------------------------------------------------------------------------------------------------------------------------------------------
@@ -70,38 +99,63 @@ public class NextPage1 : MonoBehaviour
     public void Encyclopedia_TimePriestess_Back()
     {
         Debug.Log("_Back");
-        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP1); ///build=6
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP1); ///build=8
 	}
 
 
     public void Encyclopedia_OldGuard_Back()
     {
         Debug.Log("_Back");
-        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP2); ///build=7
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP2); ///build=9
 	}
-
 
     public void Encyclopedia_BleedingNoble_Back()
     {
         Debug.Log("_Back");
-        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP3); ///build=8
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP3); ///build=10
+	}
+
+
+
+    public void Encyclopedia_Sword_Back()
+    {
+        Debug.Log("_Back");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP4); ///build=11
+	}
+
+    public void Encyclopedia_Archer_Back()
+    {
+        Debug.Log("_Back");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP5); ///build=12
+	}
+
+    public void Encyclopedia_Lance_Back()
+    {
+        Debug.Log("_Back");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP6); ///build=13
+	}
+
+    public void Encyclopedia_Wolf_Back()
+    {
+        Debug.Log("_Back");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP7); ///build=14
 	}
 
     public void Encyclopedia_SpiritDoor_Back()
     {
         Debug.Log("_Back");
-        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP4); ///build=9
-	}
-
-    public void Encyclopedia_Mimic_Back()
-    {
-        Debug.Log("_Back");
-        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP5); ///build=10
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP8); ///build=15
 	}
 
     public void Encyclopedia_ConsumingKnight_Back()
     {
         Debug.Log("_Back");
-        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP6); ///build=11
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP9); ///build=16
+	}
+
+    public void Encyclopedia_Story_Back()
+    {
+        Debug.Log("_Back");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(EncyclopediaP10); ///build=17
 	}
 }
