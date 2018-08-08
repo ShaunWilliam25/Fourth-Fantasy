7c5b8bd50b5c6fb8b939c3211d6b0527c1ce5145
LimDongHuang <39880410+LimDongHuang@users.noreply.github.com>
2018-08-08 18:34:08 +0800

    reimported the entire game + wolf pivot fixed

--- Assets/Asset/Scripts/Tutorial/TutorialAppear.cs
+++ Assets/Asset/Scripts/Tutorial/TutorialAppear.cs
@@ -19,9 +19,9 @@ public class TutorialAppear : MonoBehaviour {
     [SerializeField] List<actionTimeBar> playerAtbList;
     bool isLectureDone = false;
     bool isOrderLayerAdjusted = false;
-    [SerializeField]bool isLecture6ShowedBefore = false;
+    bool isLecture6ShowedBefore = false;
     public bool isEnemyAttacked = false;
-    [SerializeField] bool isATBListFilled = false;
+    [SerializeField] bool isPlayerATBListFilled = false;
     [SerializeField] Image campsiteTutorial;
     [SerializeField] Image needGreedTutorial;
     [SerializeField] bool isCanPress = false;
@@ -45,7 +45,7 @@ public class TutorialAppear : MonoBehaviour {
     private void Awake()
     {
         sceneManager = GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SceneManager>();
-        isATBListFilled = false;
+        isPlayerATBListFilled = false;
     }
 
     // Use this for initialization
@@ -68,11 +68,11 @@ public class TutorialAppear : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-        if(!isATBListFilled)
+        if(!isPlayerATBListFilled)
         {
             playerAtbList[0] = Player1.instance.gameObject.GetComponent<actionTimeBar>();
-            playerAtbList[1] = Player2.instance.gameObject.GetComponent<actionTimeBar>();            
-            isATBListFilled = true;
+            playerAtbList[1] = Player2.instance.gameObject.GetComponent<actionTimeBar>();
+            isPlayerATBListFilled = true;
         }
         
         //! Skip tutorial
@@ -302,10 +302,6 @@ public class TutorialAppear : MonoBehaviour {
                             lectureCanvas.transform.GetChild(5).gameObject.SetActive(true);
                             isLecture6ShowedBefore = true;
                         }
-                    }
-                    else
-                    {
-                        continue;
                     }
                 }
             }            
--- Assets/Asset/Scripts/Tutorial/TutorialTaskCheck.cs
+++ Assets/Asset/Scripts/Tutorial/TutorialTaskCheck.cs
@@ -29,6 +29,7 @@ public class TutorialTaskCheck : MonoBehaviour {
                     toggleList[i].isOn = false;
                 }
                 tutorial.tutorialStage = TutorialAppear.TUTORIAL_STAGE.STAGE_02;
+                Debug.Log("toggle is " + toggleList[0].isOn);
             }
             
         }
