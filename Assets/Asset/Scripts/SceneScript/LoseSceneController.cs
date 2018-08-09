a7357fd6d094a23a91129e0884fce67d98f3dfb3
Poh Hao Bing <poh_hao_bing@hotmail.com>
2018-08-10 02:52:25 +0800

    Tweak of Lose Scene

--- Assets/Asset/Scripts/SceneScript/LoseSceneController.cs
+++ Assets/Asset/Scripts/SceneScript/LoseSceneController.cs
@@ -2,9 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.EventSystems;
 
 public class LoseSceneController : MonoBehaviour {
+
+    public GameObject lastselect;
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastselect);
+        }
+        else
+        {
+            lastselect = EventSystem.current.currentSelectedGameObject;
+        }
+    }
 
     public void Retry()
     {
